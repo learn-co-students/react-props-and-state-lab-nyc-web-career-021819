2f0d80dc2906a32e86c9e31029dc62aa90c8c10b
Kate Kim <katekim5809@gmail.com>
2019-04-24 17:15:34 -0400

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,11 @@
 import React from 'react'
-
+// import allPets from '../data/pets'
 import Filters from './Filters'
 import PetBrowser from './PetBrowser'
 
 class App extends React.Component {
   constructor() {
     super()
-
     this.state = {
       pets: [],
       filters: {
@@ -15,7 +14,44 @@ class App extends React.Component {
     }
   }
 
+handleFindPetsClick = event => {
+    let url = '/api/pets'
+
+    if (this.state.filters.type !== 'all') {
+            url += `?type=${this.state.filters.type}`;
+          }
+
+    fetch(url)
+          .then(res => res.json())
+          .then(pets => this.setState({ pets }));
+
+}
+
+handleChangeType = event =>{
+  this.setState({
+    filters:
+          {...this.state.filters,
+          type: event.target.value}
+      })
+  }
+
+onAdoptPet = (id) =>{
+  let arr = this.state.pets.map(p => {
+    if(p.id === id){
+      return{...p, isAdopted: !p.isAdopted}}else{
+      return p
+    }
+    // p.id === id ? {...p, isAdopted: !p.isAdopted} : p
+    })
+
+this.setState({
+  pets: arr
+})
+}
+
+
   render() {
+    console.log(this.state);
     return (
       <div className="ui container">
         <header>
@@ -24,10 +60,12 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters
+               onChangeType={this.handleChangeType}
+               onFindPetsClick={this.handleFindPetsClick}/>
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} onAdoptPet={this.onAdoptPet}/>
             </div>
           </div>
         </div>
--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import './fetch-setup';
+// import allPets from './data/pets'
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
